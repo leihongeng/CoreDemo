b10c0b3fae304114f2a782845cc0c7f72ef0df0c
LeiHong <leihongeng@outlook.com>
2017-04-28 11:33:38 +0800

    addproject

---
+++ CoreDemo.Data/Models/CoreDemoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CoreDemo.Data.Entitys;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoreDemo.Data.Models
+{
+    public class CoreDemoContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=rm-wz93fh055oa154dq9o.sqlserver.rds.aliyuncs.com,3433; Database=CoreDemo; User Id=leihong; Password=P@ssw0rd;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
